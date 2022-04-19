3f68ae4359723b57d34cad9b5b849e7ef9c40803
Alexandre Fabian <xandy.fabian@gmail.com>
2022-04-19 18:56:52 -0300

    Finalizado servlet para conversao de km/h para mph

--- src/main/java/dev/amf/pw2_ex_xml/UI.java
+++ src/main/java/dev/amf/pw2_ex_xml/UI.java
@@ -7,13 +7,19 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.WebServiceRef;
 
 @WebServlet("/mph")
 public class UI extends HttpServlet {
     
+     @WebServiceRef(wsdlLocation = "http://localhost:9080/Big?wsdl")
+     private Big_Service service;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       
-    }
+        
+        Big port = service.getBigPort();
+        System.out.println("Conversão de km/h para mph " + port.conversao(10));
+     }
     
 }
