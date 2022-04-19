45c8c2915ef5bdc747e022b021927d45406ea4f9
Alexandre Fabian <xandy.fabian@gmail.com>
2022-04-19 18:02:54 -0300

    Projeto XML WS inicial

---
+++ src/main/java/dev/amf/pw2_ex_xml/Pw2_ex_xmlRestApplication.java
@@ -0,0 +1,11 @@
+package dev.amf.pw2_ex_xml;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class Pw2_ex_xmlRestApplication extends Application {
+}
